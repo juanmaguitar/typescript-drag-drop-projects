0ab0506b8835837750e2cf38e5ae4659bfce82d3
JuanMa Garrido <juanma.garrido@gmail.com>
2024-11-06 11:18:08 +0000

    Added deploy script and removed console.log

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,10 @@
 import ProjectState from "./ProjectState";
 import ProjectList from "./ProjectList";
 import ProjectInput from "./ProjectInput";
+import { ProjectStatus } from "./Project";
 
 const projectState = ProjectState.getInstance();
 
-const prjInput = new ProjectInput(projectState);
-const activePrjList = new ProjectList("active", projectState);
-const finishedPrjList = new ProjectList("finished", projectState);
-
-console.log(prjInput);
-console.log(activePrjList);
-console.log(finishedPrjList);
+new ProjectInput(projectState);
+new ProjectList(ProjectStatus.Active, projectState);
+new ProjectList(ProjectStatus.Finished, projectState);
